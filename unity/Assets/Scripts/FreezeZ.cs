cd00d4d47c5e0ad1bd26f272945f4a646129ab8b
Momin Khan <foolmoron@gmail.com>
2014-01-25 18:24:22 -0500

    Added FreezeZ component

---
+++ unity/Assets/Scripts/FreezeZ.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeZ : MonoBehaviour {
+
+	public float ZValueToFreeze = 0;
+
+	void FixedUpdate() {
+		Vector3 currentPosition = transform.position;
+		currentPosition.z = ZValueToFreeze;
+		transform.position = currentPosition;
+	}
+}
