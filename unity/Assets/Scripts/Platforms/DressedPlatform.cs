e5047142cf761a6b0dc6abf0f8dd99d1ffc5ef0e
Momin Khan <foolmoron@gmail.com>
2014-01-25 13:39:00 -0500

    Tiled platforms logic

--- unity/Assets/Scripts/Platforms/DressedPlatform.cs
+++ unity/Assets/Scripts/Platforms/DressedPlatform.cs
@@ -2,23 +2,32 @@
 using System.Collections;
 
 namespace GGJ14 {
-	[RequireComponent(typeof(SpriteRenderer))]
 	public class DressedPlatform : MonoBehaviour {
 		public SpriteRenderer SpriteRenderer { get; private set; }
+		public MeshRenderer MeshRenderer { get; private set; }
 		public Collider Collider { get; private set; }
+
+		Color fadedColor = Color.Lerp(Color.white, Color.clear, 0.75f);
 
 		void Awake() {
 			SpriteRenderer = GetComponent<SpriteRenderer>();
+			MeshRenderer = GetComponent<MeshRenderer>();
 			Collider = GetComponent<Collider>();
 		}
 
 		public virtual void Disable() {
-			SpriteRenderer.enabled = false;
+			if (SpriteRenderer) 
+				SpriteRenderer.color = fadedColor;
+			else if (MeshRenderer)
+				MeshRenderer.material.SetColor("_TintColor", Color.Lerp(fadedColor, Color.clear, 0.5f)); // particles shader requires half as much alpha
 			if (Collider) Collider.enabled = false;
 		}
 
 		public virtual void Enable() {
-			SpriteRenderer.enabled = true;
+			if (SpriteRenderer) 
+				SpriteRenderer.color = Color.white;
+			else if (MeshRenderer)
+				MeshRenderer.material.SetColor("_TintColor", Color.Lerp(Color.white, Color.clear, 0.5f));
 			if (Collider) Collider.enabled = true;
 		}
 	}
