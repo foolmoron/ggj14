23683f4c68b8b35314ae5524987538a58a2e13a0
Momin Khan <foolmoron@gmail.com>
2014-01-25 21:09:14 -0500

    Level transition system

---
+++ unity/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour {
+	public float FadeDuration = 2f;
+	float fadeTime = 0;
+	SpriteRenderer spriteRenderer;
+	Material fadeMaterial;
+
+	void Start () {
+		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+		spriteRenderer.enabled = true;
+		fadeMaterial = spriteRenderer.material;
+		fadeMaterial.color = Color.white;
+	}
+
+	void Update () {
+		if (fadeTime < FadeDuration) {
+			fadeTime += Time.deltaTime;
+			float interp = fadeTime / FadeDuration;
+			fadeMaterial.color = Color.Lerp(Color.white, Color.clear, interp);
+		} else {
+			spriteRenderer.enabled = false;
+			this.enabled = false;
+		}
+	}
+}
---
+++ unity/Assets/Scripts/Goal.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	public float FadeDelay = 1f;
+	public float FadeDuration = 2f;
+	public string NextLevel;
+	public SpriteRenderer FadeSprite;
+	public GameObject VictoryParticles;
+
+	bool fading = false;
+	float fadeTime = 0;
+	bool canLoad = true;
+
+	void Start () {
+		FadeSprite.material.color = Color.clear;
+	}
+
+	void Update () {
+		if (fading) {
+			fadeTime += Time.deltaTime;
+			float interp = fadeTime / FadeDuration;
+			if (interp >= 1) {
+				Application.LoadLevel(NextLevel);
+			}
+
+			FadeSprite.material.color = Color.Lerp(Color.clear, Color.white, interp);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (!canLoad)
+			return;
+
+		canLoad = false;
+		StartCoroutine(FadeWithDelay());
+	}
+
+	IEnumerator FadeWithDelay() {
+		Instantiate(VictoryParticles, transform.position, Quaternion.identity);
+		yield return new WaitForSeconds(FadeDelay);
+		FadeSprite.enabled = true;
+		fading = true;
+	}
+}
