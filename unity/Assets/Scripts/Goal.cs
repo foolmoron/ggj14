ec8451a66d27408772a356d730f7fe13a42e1f7d
Momin Khan <foolmoron@gmail.com>
2014-01-25 22:14:17 -0500

    Added in-between-level cutscene

--- unity/Assets/Scripts/Goal.cs
+++ unity/Assets/Scripts/Goal.cs
@@ -9,6 +9,14 @@ public class Goal : MonoBehaviour {
 	public SpriteRenderer FadeSprite;
 	public GameObject VictoryParticles;
 
+	public Sprite CutsceneSprite;
+	public float CutsceneFadeInDuration = 1f;
+	public float CutsceneDuration = 3f;
+	public float CutsceneFadeOutDuration = 1f;
+	SpriteRenderer cutscene;
+	bool cutsceneFadingIn = false;
+	bool cutsceneFadingOut = false;
+
 	bool fading = false;
 	float fadeTime = 0;
 	bool canLoad = true;
@@ -22,9 +30,30 @@ public class Goal : MonoBehaviour {
 			fadeTime += Time.deltaTime;
 			float interp = fadeTime / FadeDuration;
 			if (interp >= 1) {
-				Application.LoadLevel(NextLevel);
+				fading = false;
+				StartCutscene();
 			}
 
+			FadeSprite.material.color = Color.Lerp(Color.clear, Color.white, interp);
+		}
+		if (cutsceneFadingIn) {
+			fadeTime += Time.deltaTime;
+			float interp = fadeTime / CutsceneFadeInDuration;
+			if (interp >= 1) {
+				cutsceneFadingIn = false;
+				StartCoroutine(FadeOutCutsceneWithDelay());
+			}
+			
+			FadeSprite.material.color = Color.Lerp(Color.white, Color.clear, interp);
+		}
+		if (cutsceneFadingOut) {
+			fadeTime += Time.deltaTime;
+			float interp = fadeTime / CutsceneFadeOutDuration;
+			if (interp >= 1) {
+				cutsceneFadingOut = false;
+				Application.LoadLevel(NextLevel);
+			}
+			
 			FadeSprite.material.color = Color.Lerp(Color.clear, Color.white, interp);
 		}
 	}
@@ -42,5 +71,25 @@ public class Goal : MonoBehaviour {
 		yield return new WaitForSeconds(FadeDelay);
 		FadeSprite.enabled = true;
 		fading = true;
+	}
+
+	IEnumerator FadeOutCutsceneWithDelay() {
+		yield return new WaitForSeconds(CutsceneDuration);
+		FadeSprite.enabled = true;
+		cutsceneFadingOut = true;
+		fadeTime = 0;
+	}
+
+	void StartCutscene() {
+		var cutsceneSprite = new GameObject("CutsceneSprite");
+		var cutscene = cutsceneSprite.AddComponent<SpriteRenderer>();
+		cutscene.sprite = CutsceneSprite;
+		cutscene.sortingOrder = FadeSprite.sortingOrder;
+		cutscene.transform.parent = FadeSprite.transform.parent;
+		cutscene.transform.localPosition = new Vector3(FadeSprite.transform.localPosition.x, FadeSprite.transform.localPosition.y, FadeSprite.transform.localPosition.z + 1);
+		cutscene.transform.localScale = new Vector3(6.4f, 6.4f, 1);
+
+		cutsceneFadingIn = true;
+		fadeTime = 0;
 	}
 }
