446b669d2a7951e74acd8e703772f97bdf2e8721
Momin Khan <foolmoron@gmail.com>
2014-01-26 11:10:04 -0500

    Added death trigger to bottom of level when she falls off

---
+++ unity/Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		StartLevelReset();
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		StartLevelReset();
+	}
+
+	void StartLevelReset() {
+		Debug.Log("Loading");
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
